3b5b7de0200da2079029f84a41ce99498685b1c1
GitHub Action <action@github.com>
2022-01-06 02:39:37 +0000

    Template cleanup

--- src/main/kotlin/com/github/zhangliangliangs/mvvmplugintemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/zhangliangliangs/mvvmplugintemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.zhangliangliangs.mvvmplugintemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.zhangliangliangs.mvvmplugintemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
---
+++ src/main/kotlin/com/github/zhangliangliangs/mvvmplugintemplate/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.zhangliangliangs.mvvmplugintemplate.services
+
+import com.github.zhangliangliangs.mvvmplugintemplate.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
